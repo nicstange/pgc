feab1f59fa4975ef91ddb1496c2379804b33d013
Nicolai Stange <nstange@suse.de>
2019-02-20 09:56:13 +0100

    add license statements
    Add a copy of the GPLv2 as well as file header comments to all source
    files.
    
    Signed-off-by: Nicolai Stange <nstange@suse.de>

--- victim-checker.h
+++ victim-checker.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2019 SUSE LLC
+ *
+ * This file is part of pgc.
+ *
+ * pgc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2.
+ *
+ * pgc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pgc.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef _VICTIM_CHECKER_H
 #define _VICTIM_CHECKER_H
 
